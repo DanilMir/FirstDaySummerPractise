4b21a5e60f8b395f136b1f448fabcf2367c3a007
HotBlender <Danil@DESKTOP-CLT0SUH>
2021-07-01 23:09:09 +0300

    login activity

--- app/src/main/java/com/itis/firstdaysummerpructise/MainActivity.kt
+++ app/src/main/java/com/itis/firstdaysummerpructise/MainActivity.kt
@@ -2,10 +2,30 @@ package com.itis.firstdaysummerpructise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvPass: TextView? = null
+    private var tvEmail: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var email = intent?.extras?.getString("EXTRA_EMAIL")
+        var password = intent?.extras?.getString("EXTRA_PASS")
+
+        tvPass = findViewById(R.id.tv_password)
+        tvEmail = findViewById(R.id.tv_email)
+
+        if(email != null)
+        {
+            tvEmail?.text = email
+        }
+        if(password != null)
+        {
+            tvPass?.text = password
+        }
     }
 }
