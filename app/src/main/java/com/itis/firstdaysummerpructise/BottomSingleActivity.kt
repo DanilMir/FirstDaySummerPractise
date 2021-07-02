ef047e040646dc9a759a90d831b750f6b3684feb
HotBlender <Danil@DESKTOP-CLT0SUH>
2021-07-02 14:49:05 +0300

    bottom acticity

---
+++ app/src/main/java/com/itis/firstdaysummerpructise/BottomSingleActivity.kt
@@ -0,0 +1,21 @@
+package com.itis.firstdaysummerpructise
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class BottomSingleActivity: AppCompatActivity() {
+
+    private lateinit var controller: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_bottom)
+
+        controller = (supportFragmentManager.findFragmentById(R.id.host_fragment) as NavHostFragment).navController
+        val bnvMain = findViewById<BottomNavigationView>(R.id.bnv_main)
+        bnvMain.setupWithNavController(controller)
+    }
+}
